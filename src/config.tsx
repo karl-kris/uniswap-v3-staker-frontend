4c2fe063e8698892244b885b78ad5b1a5224329d
Mchain <marbellachain@gmail.com>
2024-04-07 01:07:07 +0200

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -9,33 +9,52 @@ export const IS_DEV = 1;
 export const CACHE_WALLET_KEY = 'wallet';
 
 export const NETWORK_MAINNET = 'mainnet';
-export const NETWORK_SEPOLIA = 'sepolia';
-export const AVAILABLE_NETWORKS = [NETWORK_SEPOLIA];
+export const NETWORK_MUMBAI = 'maticmum';
+export const NETWORK_ARBITRUM = 'arbitrum';
+export const AVAILABLE_NETWORKS = [NETWORK_MUMBAI];
+
+export const EXPLORER_URLS: Record<string, string> = {
+  [NETWORK_MAINNET]: 'https://etherscan.io',
+  [NETWORK_MUMBAI]: 'https://mumbai.polygonscan.com',
+  [NETWORK_ARBITRUM]: 'https://arbiscan.io',
+};
+
+export const CHAIN_IDS: Record<string, number> = {
+  [NETWORK_MAINNET]: 1,
+  [NETWORK_MUMBAI]: 80001,
+  [NETWORK_ARBITRUM]: 42161,
+};
 
 export const TOKEN_0_ADDRESS: Record<string, string> = {
   [NETWORK_MAINNET]: '',
-  [NETWORK_SEPOLIA]: '0x8aE7de3a5cFe804E4b4adD9f2881CB90042CBcE0',
+  [NETWORK_MUMBAI]: '0x8aE7de3a5cFe804E4b4adD9f2881CB90042CBcE0',
+  [NETWORK_ARBITRUM]: '',
 };
 
 // WETH
 export const TOKEN_1_ADDRESS: Record<string, string> = {
   [NETWORK_MAINNET]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  [NETWORK_SEPOLIA]: '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14',
+  [NETWORK_MUMBAI]: '0x005E69B83Ea045836Bca630a2b46D64023f3cb70',
+  [NETWORK_ARBITRUM]: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
 };
 
 export const NFT_POSITIONS_MANAGER_ADDRESS: Record<string, string> = {
   [NETWORK_MAINNET]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
-  [NETWORK_SEPOLIA]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+  [NETWORK_MUMBAI]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+  [NETWORK_ARBITRUM]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
 };
 
 export const STAKING_REWARDS_ADDRESS: Record<string, string> = {
   [NETWORK_MAINNET]: '0xe34139463bA50bD61336E0c446Bd8C0867c6fE65',
-  [NETWORK_SEPOLIA]: '0xe34139463bA50bD61336E0c446Bd8C0867c6fE65',
+  [NETWORK_MUMBAI]: '0xe34139463bA50bD61336E0c446Bd8C0867c6fE65',
+  [NETWORK_ARBITRUM]: '0xe34139463bA50bD61336E0c446Bd8C0867c6fE65',
 };
 
 export const SUBGRAPHS: Record<string, string> = {
   [NETWORK_MAINNET]:
     'https://api.thegraph.com/subgraphs/name/mchainnetwork/mark-staking-arbitrum',
-  [NETWORK_SEPOLIA]:
-    'https://api.thegraph.com/subgraphs/name/mchainnetwork/mark-staking-sepolia',
+  [NETWORK_MUMBAI]:
+    'https://api.thegraph.com/subgraphs/name/mchainnetwork/mark-staking-mumbai',
+  [NETWORK_ARBITRUM]:
+    'https://api.thegraph.com/subgraphs/name/mchainnetwork/mark-staking-arbitrum',
 };
